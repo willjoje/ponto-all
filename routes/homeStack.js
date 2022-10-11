51706df657afc0904db44016cfbbff26caad7c2b
willjoje <jefwillyam>
2022-10-11 12:26:47 -0300

    header styling

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -4,22 +4,35 @@ import Home from "../screens/home";
 import Guide from "../screens/guide";
 import FaceVerification from "../screens/faceVerification";
 import Confirmation from "../screens/confirmation";
+import { WhiteBalance } from "expo-camera";
 
 const screens = {
   Home: {
     screen: Home,
-  },
-  Confirmation: {
-    screen: Confirmation,
+    navigationOptions: { headerTintColor: "#fff" },
   },
   Guide: {
     screen: Guide,
+    navigationOptions: { title: "Reconhecimento facial" },
   },
   FaceVerification: {
     screen: FaceVerification,
+    navigationOptions: { title: "Reconhecimento facial" },
+  },
+  Confirmation: {
+    screen: Confirmation,
+    navigationOptions: { headerTintColor: "#fff" },
   },
 };
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerShown: true,
+    headerStyle: {
+      backgroundColor: "white",
+      shadowColor: "transparent",
+    },
+  },
+});
 
 export default createAppContainer(HomeStack);
